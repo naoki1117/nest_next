c2eb46e779d52ae18f0cb1fba6a1e8e9f4ebad5c
naoki1117 <105569021+naoki1117@users.noreply.github.com>
2022-08-24 19:02:02 +0900

    aaa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,11 +9,11 @@ export default class _Document extends Document {
   render() {
     return (
       <Html>
-        <Head>
+        <head>
           <link rel='manifest' href='/manifest.json' />
           <link rel='apple-touch-icon' href='/icon.png' ></link>
           <meta name='theme-color' content='#fff'/>
-        </Head>
+        </head>
 
         <Head/>
         <body>
